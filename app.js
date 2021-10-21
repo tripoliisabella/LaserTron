008ae1de76d364ecb8600ba669799bfeee320b4a
tripoliisabella <tripoli.isabella@gmail.com>
2021-10-20 21:53:02 -0400

    fixed bug with port

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ const path = require('path');
 //uses the public folder as static.
 app.use(express.static('views'));
 //sets the views folders where all the ejs templates are located
-app.set('view engine', 'ejs');
-//pulling in the blogpost collection
-const BlogPost = require('./models/BlogPost');
-mongoose.connect('mongodb://localhost/my_database',{useNewURLParser:true})
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+let port = process.env.PORT; 
 app.listen(port, () => {
     console.log("App is listening on port 8082");
 });
 
-let port = process.env.PORT; 
+
 
 if (port == null || port == ""){
    port = 8082;
